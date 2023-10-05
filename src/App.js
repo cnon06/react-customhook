258dd0bdf429754e947893463bd0b8910165c25d
Sinan Alagöz <sinanalagoz74@gmail.com>
2023-10-05 20:39:23 +0300

    33

--- src/App.js
+++ src/App.js
@@ -2,39 +2,68 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [press, setPress] = useState(false);
-  const [press2, setPress2] = useState(false);
+  const [amount, setAmount] = useState(700);
+  const [rate, setRate] = useState(0);
+  const [fromCur, setFromCur] = useState("USD");
+  const [toCur, setToCur] = useState("USD");
+  const [isLoading, setIsLoading] = useState(false);
 
-  // when the component render firstly
+  const getData = async () => {
+    setIsLoading(true);
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`
+    );
 
-  useEffect(() => {
-    console.log("A");
-  }, []);
-
-  // it works with any render
-  useEffect(() => {
-    console.log("B");
-  });
-
-  // it works when only press2 state is set
-  useEffect(() => {
-    console.log("C");
-  }, [press2]);
-
-  const handleClick = () => {
-    setPress(!press);
-    console.log(press);
+    const data = await res.json();
+    const rates = data.rates["USD"];
+    // console.log(data.rates["USD"]);
+    setRate(Number(rates));
+    setIsLoading(false);
   };
 
-  const handleClick2 = () => {
-    setPress2(!press2);
-    console.log(press2);
+  useEffect(() => {
+    if (fromCur === toCur) return setRate(amount);
+    getData();
+  }, [amount, fromCur, toCur]);
+
+  const handleCalculate = async (e) => {
+    setAmount(e);
   };
 
   return (
     <div className="App">
-      <button onClick={handleClick}>Press</button>
-      <button onClick={handleClick2}>Press</button>
+      <input
+        type="text"
+        onChange={(e) => handleCalculate(e.target.value)}
+        // disabled={isLoading}
+      />
+      from
+      <select
+        name=""
+        id=""
+        onChange={(e) => setFromCur(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      to
+      <select
+        name=""
+        id=""
+        onChange={(e) => setToCur(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <h5>
+        from {fromCur} to {toCur} {rate}
+      </h5>
     </div>
   );
 }
